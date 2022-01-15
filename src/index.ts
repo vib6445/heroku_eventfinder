b87670b8f3166b381611fcae31fde0c9d3c8edd1
vib6445 <vingeobrun@googlemail.com>
2022-01-15 15:01:52 +0100

    Remove nodemon for heroku commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+
 import { connectToDatabase } from './database/database-connection';
 import { userRoute } from './routes/user.route';
 //jsonwebtoken
